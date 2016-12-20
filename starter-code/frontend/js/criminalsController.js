f265772e32a3acd897fc75e885ae7d7ba6866b82
erikisaac <erik@erikisaac.com>
2016-12-20 09:00:16 -0700

    First commit.

---
+++ starter-code/frontend/js/criminalsController.js
@@ -0,0 +1,34 @@
+// angular.module('TheCriminalsApp', [])
+//   .controller('CriminalsController', CriminalsController);
+
+// CriminalsController.$inject = ['$http'];
+
+// function CriminalsController($http){
+//   var self = this;
+//   self.all = [];
+//   this.addCriminal = addCriminal;
+//   this.newCriminal = {};
+//   self.getCriminal = getCriminal;
+//   // self.deletePresident = deletePresident;
+
+// function getCriminal(){
+//   $http
+//     .get('http://localhost:3000/criminal')
+//     .then(function(response) {
+//       self.all = response.data.Criminal;
+//     });
+// }
+
+//   getCriminal();
+
+
+//   function addCriminal(){
+//   $http
+//     .post('http://localhost:3000/criminal', self.newCriminal)
+//     .then(function(response){
+//       getCriminals();
+//     });
+//     this.newCriminal = {};
+//   }
+// }
+
